cad134651d9a187f7eea25fb781123e41e630fca
adejui <adejuinurrochim@gmail.com>
2025-03-01 18:58:13 +0700

    Template Refs

---
+++ src/components/Note.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>Buat Note</h1>
+        <input ref="noteInput" type="text" name="note" v-model="note" placeholder="Tulis Note Disini" autofocus/>
+        <button @click="addNote">Tambah note</button>
+    </div>
+
+    <h1>Daftar Note</h1>
+    <ul>
+        <li v-for="note in notes" ref="noteList">{{ note }}</li>
+    </ul>
+</template>
+
+<script setup>
+import { reactive, useTemplateRef, ref } from "vue";
+
+const notes = reactive([]);
+const note = ref("");
+const noteInput = useTemplateRef("noteInput");
+const noteList = useTemplateRef("noteList");
+
+function addNote() {
+    notes.push(note.value);
+    note.value = "";
+    noteInput.value.focus();
+    if(noteList.value) {
+        noteList.value.forEach((li) => {
+            console.log(li.textContent);
+        })
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
