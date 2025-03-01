c76fe0f97fe7904243b5f47ee5872bc7283b54b6
adejui <adejuinurrochim@gmail.com>
2025-03-01 17:19:08 +0700

    Watchers

---
+++ src/components/Product.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <label for="productId"
+            >Product Id
+            <select id="productId" v-model="productId">
+                <option value="product1">Product 1</option>
+                <option value="product2">Product 2</option>
+                <option value="product3">Product 3</option>
+            </select>
+        </label>
+    </div>
+
+    <div v-if="product">
+        <h1>Product</h1>
+        <p>Id : {{ product.id }}</p>
+        <p>Name : {{ product.name }}</p>
+        <p>Price : {{ product.price }}</p>
+    </div>
+</template>
+
+<script setup>
+import { onWatcherCleanup, ref, watch, watchEffect } from "vue";
+
+const productId = ref("product1");
+const product = ref(null);
+
+// watch(
+//     productId,
+//     async (newVal, oldVal) => {
+//         console.log("call watch callback");
+//         const response = await fetch(`/${newVal}.json`);
+//         product.value = await response.json();
+//     },
+//     {
+//         immediate: true,
+//     }
+// );
+watchEffect(async (newVal, oldVal) => {
+    onWatcherCleanup(() => {
+        console.log("clean up");
+    });
+
+    console.log("call watch callback");
+    const response = await fetch(`/${productId.value}.json`);
+    product.value = await response.json();
+});
+</script>
+
+<style lang="scss" scoped></style>
