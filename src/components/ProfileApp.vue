d627344d00e795a065cafd47c6a3d43c72c39019
adejui <adejuinurrochim@gmail.com>
2025-03-02 14:15:21 +0700

    Provide dan Inject

---
+++ src/components/ProfileApp.vue
@@ -0,0 +1,20 @@
+<template>
+    <input type="text" v-model="profile.name" placeholder="Name" />
+    <Profile />
+    <ProfileAddress />
+</template>
+
+<script setup>
+import { provide, reactive } from "vue";
+import Profile from "./Profile.vue";
+import ProfileAddress from "./ProfileAddress.vue";
+
+const profile = reactive({
+    name: "Ade",
+    email: "test@gmail.com",
+});
+
+provide("profile", profile);
+</script>
+
+<style lang="scss" scoped></style>
